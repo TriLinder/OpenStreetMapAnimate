17050fd46562ef4684a27c8d5eb87293a2f7b7e3
TriLinder <82846064+TriLinder@users.noreply.github.com>
2023-06-04 15:24:42 +0200

    Barebones add user modal

---
+++ src/lib/Ui/AddUserModal/AddUserModal.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { Button, Modal, ModalHeader, ModalFooter, ModalBody } from "sveltestrap";
+
+    export let isOpen = false;
+
+    function toggle() {
+        isOpen =! isOpen;
+    }
+</script>
+
+<Modal isOpen={isOpen}>
+    <ModalHeader {toggle}> Add user </ModalHeader>
+
+    <ModalBody>
+        <p>Step 1</p>
+    </ModalBody>
+
+    <ModalFooter>
+        <Button color="danger" on:click={toggle}>Cancel</Button>
+        <Button color="primary">Continue</Button>
+    </ModalFooter>
+</Modal>
--- src/lib/Ui/Ui.svelte
+++ src/lib/Ui/Ui.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+    import AddUserModal from "./AddUserModal/AddUserModal.svelte";
     import BottomHud from "./BottomHUD.svelte";
 </script>
 
+<AddUserModal isOpen={true}/>
 <BottomHud/>
